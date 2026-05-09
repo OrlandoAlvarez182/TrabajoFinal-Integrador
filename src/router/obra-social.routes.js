1943386a2f82b76b4a86366c790e48e22d1592cc
Avila Esteban <40841825+CQaS@users.noreply.github.com>
2026-05-08 21:45:59 -0300

    validar en crear

--- src/router/obra-social.routes.js
+++ src/router/obra-social.routes.js
@@ -10,11 +10,11 @@ const obraSocialRouter = Router();
 
 const OScontroller = new ObraSocialController();
 
- obraSocialRouter.get('/', OScontroller.obtenerTodas);
- obraSocialRouter.get('/:id', OScontroller.obraSocialPorID);
- obraSocialRouter.post('/', OScontroller.create);
- obraSocialRouter.put('/:id', validarObraSocial, OScontroller.obraSocialActualizar);
- obraSocialRouter.delete('/:id', OScontroller.borrarObraSocial);
+obraSocialRouter.get('/', OScontroller.obtenerTodas);
+obraSocialRouter.get('/:id', OScontroller.obraSocialPorID);
+obraSocialRouter.post('/', validarObraSocial, OScontroller.create);
+obraSocialRouter.put('/:id', validarObraSocial, OScontroller.obraSocialActualizar);
+obraSocialRouter.delete('/:id', OScontroller.borrarObraSocial);
 
 
 export default obraSocialRouter;
